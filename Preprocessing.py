6eafc06a57f10564506a1ef27de5da0caf0524b0
Anushka Chaudhuri <59223517+anushka0301@users.noreply.github.com>
2020-08-29 00:18:15 +0530

    Add files via upload

---
+++ Preprocessing.py
@@ -0,0 +1,21 @@
+#importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Importing the datasets
+dataset=pd.read_csv('Data.csv')
+X=dataset.iloc[:, :-1].values
+Y=dataset.iloc[:, 3].values
+
+#Splitting the dataset into Training and Test sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test=train_test_split(X, Y, test_size=0.2, random_state=0)
+
+#Feature scaling
+"""
+from sklearn.preprocessing import StandardScaler
+sc_X=StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.transform(X_test)
+"""
