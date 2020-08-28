402f32fa97dbc9420d3b11730d56c83b94c57a9f
Anushka Chaudhuri <59223517+anushka0301@users.noreply.github.com>
2020-08-29 00:26:15 +0530

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -4,9 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #Importing the datasets
-dataset=pd.read_csv('Data.csv')
+dataset=pd.read_csv('Salary_Data.csv')
 X=dataset.iloc[:, :-1].values
-Y=dataset.iloc[:, 3].values
+Y=dataset.iloc[:, -1].values
 
 #Splitting the dataset into Training and Test sets
 from sklearn.model_selection import train_test_split
